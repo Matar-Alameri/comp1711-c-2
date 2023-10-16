f9014175114d06c8452fa971a84e497c9e1f298d
Matar-Alameri <146854829+Matar-Alameri@users.noreply.github.com>
2023-10-16 22:03:42 +0000

    finished task 2

---
+++ task1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    int input1, input2;
+    printf("enter first number: ");
+    scanf("%d", &input1);
+    printf("enter second number: ");
+    scanf("%d", &input2);
+    
+    printf("the sum is %d\n", input1 + input2);
+
+    return 0;
+}
